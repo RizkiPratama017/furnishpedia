697b5aaf68bee7df39070d2ce96700ab4a3cbe34
dimashidayatulloh <dimaskalem07@gmail.com>
2024-12-01 14:06:30 +0700

    halaman dashboard penjual v1

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -10,19 +10,19 @@ class cart extends Model
     /** @use HasFactory<\Database\Factories\CartFactory> */
     use HasFactory;
 
-        protected $table = 'cart';
+    protected $table = 'cart';
 
-        protected $fillable = [
-            'quantity',
-        ];
+    protected $fillable = [
+        'quantity',
+    ];
 
-        public function user()
-        {
-            return $this->belongsTo(User::class);
-        }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
-        public function product()
-        {
-            return $this->belongsTo(Product::class);
-        }
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
