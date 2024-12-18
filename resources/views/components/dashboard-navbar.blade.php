c61c07fa643eeb4cbb43c97c2a75057792152831
dimashidayatulloh <dimaskalem07@gmail.com>
2024-12-19 00:01:19 +0700

    logo furnishpedia

--- resources/views/components/dashboard-navbar.blade.php
+++ resources/views/components/dashboard-navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class=" bg-gray-800 text-white">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="{{ asset('img/r.jpg') }}" class="h-8 rounded" alt="Ruma.id Logo" />
+            <img src="{{ asset('img/fp.jpg') }}" class="h-8 rounded" alt="Ruma.id Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Furnishpedia</span>
         </a>
         <button data-collapse-toggle="navbar-hamburger" type="button"
