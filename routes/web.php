bb141ce7ec1a94c4d0da4c4ed2245d61e333c8ba
dimashidayatulloh <dimaskalem07@gmail.com>
2024-12-31 17:30:02 +0700

    coba hapus file model buat ganti nama

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,8 @@ use App\Http\Controllers\TokoController;
 //------------------Middleware Role == all
 
 Route::get('/', function () {
-    $products = App\Models\Product::inRandomOrder()->take(6)->get();
-    $categories = App\Models\Category::all();
+    $products = Product::inRandomOrder()->take(6)->get();
+    $categories = Category::all();
     return view('home', [
         'title' => 'Home Page',
         'products' => $products,
